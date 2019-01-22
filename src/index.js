cdd12662f899170fe6fadf6f293b96790237668b
Aurimas <aurimas@darguzis.com>
2019-01-22 18:35:42 +0000

    added styling and phrase according to the season

--- src/index.js
+++ src/index.js
@@ -7,28 +7,27 @@ class App extends React.Component {
         super(props)
         this.state = { 
             lat: null,
-            errorMessage: ''
+            errorMessage: null
          }
-        
+    }
+
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({lat: position.coords.latitude})
-            },
-            (err) => this.setState({errorMessage: err.message})
+            position => this.setState({lat: position.coords.latitude}),
+            err => this.setState({errorMessage: err.message})
         )
     }
 
     render() {
-        
-        
-        return (
-            <div>
-                Latitude: {this.state.lat}
-                <br />
-                Error: {this.state.errorMessage}
-            </div>
-            )
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
 
+        if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat} />
+        }
+
+        return <div>Loading!</div>
     }
 }
     
