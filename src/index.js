fdb48904d46e79283f550d21c7ad179d90223b07
Aurimas <aurimas@darguzis.com>
2019-01-23 21:50:03 +0000

    added wrapper around whole app

--- src/index.js
+++ src/index.js
@@ -19,16 +19,22 @@ class App extends React.Component {
         )
     }
 
+    renderContent() {
+      if (this.state.errorMessage && !this.state.lat) {
+        return <div>Error: {this.state.errorMessage}</div>
+      }
+      if (!this.state.errorMessage && this.state.lat) {
+        return <SeasonDisplay lat={this.state.lat} />
+      }
+      return <Spinner message={"Please accept location request"} />
+    }
+
     render() {
-        if (this.state.errorMessage && !this.state.lat) {
-            return <div>Error: {this.state.errorMessage}</div>
-        }
-
-        if (!this.state.errorMessage && this.state.lat) {
-            return <SeasonDisplay lat={this.state.lat} />
-        }
-
-        return <Spinner message={"Please accept location request"} />
+        return (
+          <div className="border red">
+            {this.renderContent()}
+          </div>
+        )
     }
 }
     
